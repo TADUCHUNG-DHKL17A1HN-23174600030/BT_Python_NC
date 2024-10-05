5f2f73953c1ef7329ccb1d3a41c85e6033d7c6a5
khoa hoc du lieu 17A1 <tdhung.dhkl17a1hn@sv.uneti.edu>
2024-10-05 18:11:26 +0700

    bài tâp chương 2 từ 2.1 đến 2.5

---
+++ Bài tập chương 2/bài 2.4.py
@@ -0,0 +1,10 @@
+from xml.dom import minidom
+
+# Tải file XML
+doc = minidom.parse('sample.xml')
+
+# in ra node name và node đầu tiên trong file
+print(doc.nodeName)
+print(doc.firstChild.tagName)
+
+
